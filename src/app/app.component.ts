d2abe94c16d80b59b05a4f119782ce2849dea015
vinicio.nagel <vinicionagel@gmail.com>
2021-05-12 20:35:53 -0300

    commit inicial

---
+++ src/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root', //significa que é o componente que vai ser carregado no index seletor...
+  templateUrl: 'app.componente.html'
+})
+export class AppComponent {
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './componentes/template/header/header.component';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FooterComponent } from './componentes/template/footer/footer.component';
+import { NavComponent } from './componentes/template/nav/nav.component'
+
+import {MatSidenavModule} from '@angular/material/sidenav'
+import {MatListModule} from "@angular/material/list";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent
+  ],
+  imports: [
+    BrowserModule,
+    MatToolbarModule,
+    AppRoutingModule,
+    MatSidenavModule,
+    MatListModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
